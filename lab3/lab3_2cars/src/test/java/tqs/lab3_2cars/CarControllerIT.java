c189d0f0729b1a167467504dedda531895fb6026
Diogo Monteiro <diogo.mo.monteiro@ua.pt>
2022-03-27 23:17:20 +0100

    lab 3.3 done

--- lab3/lab3_2cars/src/test/java/tqs/lab3_2cars/CarControllerIT.java
+++ lab3/lab3_2cars/src/test/java/tqs/lab3_2cars/CarControllerIT.java
@@ -3,7 +3,7 @@ package tqs.lab3_2cars;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -12,13 +12,15 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase
+//@AutoConfigureTestDatabase
+@TestPropertySource( locations = "application-integrationtest.properties")
 public class CarControllerIT {
     
     @LocalServerPort
