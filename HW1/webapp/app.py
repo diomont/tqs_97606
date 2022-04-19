e436fca27d363efbb5d4b43efd3f7af970be817f
Diogo Monteiro <diogo.mo.monteiro@ua.pt>
2022-04-19 22:43:52 +0100

    Starts webapp implementation

--- HW1/webapp/app.py
+++ HW1/webapp/app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+
 
 app = Flask(__name__)
 
+BASE_URI = "http://localhost:8080/api/v1/"
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    region_name = request.args.get("regionname", "Global")
+    region_iso = request.args.get("region", "global")
+    date = request.args.get("date", None)
+
+    regions = requests.get(BASE_URI + "regions").json()
+
+    params = {}
+    if region_name != "Global" and region_iso != "global":
+        params["iso"] = region_iso
+    if date:
+        params["date"] = date
+
+    data = requests.get(BASE_URI + "incidence", params=params).json()
+
+    return render_template("index.html", regions=regions, data=data, region_name = region_name)
