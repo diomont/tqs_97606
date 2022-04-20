fe8523b21f99255409d561ea720f1a0e008706c6
Diogo Monteiro <diogo.mo.monteiro@ua.pt>
2022-04-20 16:44:59 +0100

    Implements frontend

--- HW1/webapp/app.py
+++ HW1/webapp/app.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from flask import Flask, render_template, request
 import requests
 
@@ -11,6 +12,9 @@ def index():
     region_name = request.args.get("regionname", "Global")
     region_iso = request.args.get("region", "global")
     date = request.args.get("date", None)
+    start_date = request.args.get("start", None)
+    end_date = request.args.get("end", None)
+
 
     regions = requests.get(BASE_URI + "regions").json()
 
@@ -19,7 +23,13 @@ def index():
         params["iso"] = region_iso
     if date:
         params["date"] = date
+    elif start_date and end_date:
+        params["start"] = start_date
+        params["end"] = end_date
 
-    data = requests.get(BASE_URI + "incidence", params=params).json()
+    
+    data = requests.get(BASE_URI + "incidence", params=params)#.json()
+    print(data)
+    data = data.json()
 
-    return render_template("index.html", regions=regions, data=data, region_name = region_name)
+    return render_template("index.html", regions=regions, data=data, region_name = region_name, date=date, start_date=start_date, end_date=end_date)
