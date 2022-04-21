511c5e0902bd4b265c3773c7087d25aef2651914
Diogo Monteiro <diogo.mo.monteiro@ua.pt>
2022-04-21 20:11:52 +0100

    Frontend tests, but not working :///

---
+++ HW1/api/src/test/java/tqs/hw/covidtracker/StepDefinitions.java
@@ -0,0 +1,88 @@
+package tqs.hw.covidtracker;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class StepDefinitions {
+
+
+    private WebDriver driver;
+    private boolean isPeriodTab;
+
+    @Before
+    public void setup() {
+        isPeriodTab = false;
+    }
+
+
+    @When("I navigate to {string}")
+    public void iNavigateTo(String url) {
+        driver = new FirefoxDriver();
+        driver.get(url);
+    }
+
+    @When("I select {string} as the region")
+    public void iSelectRegion(String region) {
+        driver.findElement(By.id("regionSelect")).click();
+        {
+            WebElement dropdown = driver.findElement(By.id("regionSelect"));
+            dropdown.findElement(By.xpath("//option[. = '" + region + "']")).click();
+        }
+    }
+
+    @When("I enter {string} on the date picker")
+    public void iEnterDate(String date) {
+        driver.findElement(By.id("datePicker")).click();
+        driver.findElement(By.id("datePicker")).sendKeys(date);
+    }
+
+    @When("I click the Period tab")
+    public void iClickThePeriodTab() {
+        driver.findElement(By.id("period-tab")).click();
+    }
+
+    @When("I enter {string} on the start date picker")
+    public void iEnterStartDate(String date) {
+        driver.findElement(By.id("startDatePicker")).click();
+        driver.findElement(By.id("startDatePicker")).sendKeys(date);
+    }
+
+    @When("I enter {string} on the end date picker")
+    public void iEnterEndDate(String date) {
+        driver.findElement(By.id("endDatePicker")).click();
+        driver.findElement(By.id("endDatePicker")).sendKeys(date);
+    }
+
+    @When("I click the Lookup button")
+    public void iLookup() {
+        if (isPeriodTab)
+            driver.findElement(By.xpath("//div[@id=\'periodContent\']/button")).click();
+        else
+            driver.findElement(By.xpath("//div[@id=\'dateContent\']/button")).click();
+    }
+
+    @Then("I should see {string} in the results")
+    public void iShouldSee(String expect) {
+        assertThat(driver.findElement(By.cssSelector(".mb-4")).getText())
+        .contains(expect);
+    }
+
+
+    @After
+    public void cleanup(){
+        driver.quit();
+    }
+
+}
---
+++ HW1/api/src/test/resources/tqs/hw/covidtracker/webapp.feature
@@ -0,0 +1,41 @@
+Feature: Lookup COVID-19 data on the website
+
+    Scenario: Lookup global data for a given day
+        When I navigate to "http://localhost:5000"
+        And I enter "2022-04-19" on the date picker
+        And I click the Lookup button
+        Then I should see "Global" in the results
+        And I should see "2022-04-19" in the results
+
+    Scenario: Lookup global data for a period of time
+        When I navigate to "http://localhost:5000"
+        And I click the Period tab
+        And I enter "2022-04-17" on the start date picker
+        And I enter "2022-04-19" on the end date picker
+        And I click the Lookup button
+        Then I should see "Global" in the results
+        And I should see "2022-04-17 to 2022-04-19" in the results
+    
+    Scenario: Lookup region data
+        When I navigate to "http://localhost:5000"
+        And I select "Portugal" as the region
+        And I click the Lookup button
+        Then I should see "Portugal" in the results
+    
+    Scenario: Lookup region data for a given day
+        When I navigate to "http://localhost:5000"
+        And I select "Portugal" as the 
+        And I enter "2022-04-19" on the date picker
+        And I click the Lookup button
+        Then I should see "Portugal" in the results
+        And I should see "2022-04-19" in the results
+    
+    Scenario: Lookup region data for a period of time
+        When I navigate to "http://localhost:5000"
+        And I select "Portugal" as the region
+        And I click the Period tab
+        And I enter "2022-04-17" on the start date picker
+        And I enter "2022-04-19" on the end date picker
+        And I click the Lookup button
+        Then I should see "Portugal" in the results
+        And I should see "2022-04-17 to 2022-04-19" in the results
